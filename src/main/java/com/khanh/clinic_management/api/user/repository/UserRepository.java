c63bdba6c673047801b88da76f6ab3ec3c2afa9c
Khanh091 <nvkhanh0911@gmail.com>
2026-01-24 01:26:03 +0700

    feat: add findByEmail to UserRepository and PasswordEncoder to SecurityConfig

--- src/main/java/com/khanh/clinic_management/api/user/repository/UserRepository.java
+++ src/main/java/com/khanh/clinic_management/api/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.khanh.clinic_management.api.user.repository;
 import com.khanh.clinic_management.api.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
 }
