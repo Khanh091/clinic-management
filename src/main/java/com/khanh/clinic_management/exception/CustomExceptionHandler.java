92ef65e0ebba613782f412612db4ee4373a74eb5
Khanh091 <nvkhanh0911@gmail.com>
2026-01-24 01:27:58 +0700

    feat: add custom exceptions for auth (BadRequestException, UnauthorizedException)

--- src/main/java/com/khanh/clinic_management/exception/CustomExceptionHandler.java
+++ src/main/java/com/khanh/clinic_management/exception/CustomExceptionHandler.java
@@ -3,7 +3,6 @@ package com.khanh.clinic_management.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -13,5 +12,17 @@ public class CustomExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundException ex, WebRequest req) {
         return new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequestException(BadRequestException ex, WebRequest req) {
+        return new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleUnauthorizedException(UnauthorizedException ex, WebRequest req) {
+        return new ErrorResponse(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
